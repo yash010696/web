5a55ccac7ae7788ac92362247ab63ec7a8d50fb2
Ashish <Ashish@espl.local>
2018-06-27 10:45:41 +0530

    changes in models

--- routers/requestorders.js
+++ routers/requestorders.js
@@ -4,7 +4,7 @@ var passport = require('passport');
 require('./../config/passport')(passport);
 var config = require('./../config/config');
 
-var  RequestOrder  = require('./../models/requestorder');
+var RequestOrder = require('./../models/requestorder');
 var { OrderStatus } = require('./../models/orderstatus');
 var Customer = require('./../models/customer');
 var Franchise = require('./../models/franchise');
@@ -12,6 +12,8 @@ var area = require('./../models/area');
 var generateSms = require('./../middlewear/sms');
 var generateMail = require('./../middlewear/mail');
 var serviceType = require('./../models/servicetype');
+var Timeslot = require('./../models/timeslot');
+
 
 var requestordersRouter = express.Router();
 
@@ -23,7 +25,6 @@ requestordersRouter
         if (!req.body) {
             res.status(200).json({ Success: false, Message: 'Please Enter Required Data.' });
         } else {
-
             var token = req.header('Authorization').split(' ');
             var decoded = jwt.verify(token[1], config.secret);
             var name; var mobile; var email;
@@ -50,11 +51,8 @@ requestordersRouter
                 mobile = customer.mobile;
                 email = customer.email;
 
-                var counter;
-                var orderid;
-                var areacode;
-                Franchise.find({ statee: true, area: req.body.area }).
-                    populate('area').
+                var counter; var orderid; var store_code;
+                Franchise.find({ statee: true, area: { $in: [req.body.area] } }).
                     exec(function (err, franchises) {
                         if (err) {
                             res.status(500).send({ Success: flase, err });
@@ -62,58 +60,60 @@ requestordersRouter
                         }
                         // console.log('The Franchise  is', franchises);
                         franchisename = franchises[0].franchise_Name;
-                        areacode = franchises[0].area.code;
-                        RequestOrder.find({ franchise: franchises[0]._id }).exec(function (err, results) {
+                        store_code = franchises[0].store_code;
+
+                        RequestOrder.find({ 'franchise': franchises[0]._id }).exec(function (err, results) {
                             var count = results.length;
                             counter = count + 1;
                             var str = "" + counter;
                             var pad = "0000";
                             var ans = pad.substring(0, pad.length - str.length) + str;
-                            requestId = areacode + ans;
+                            requestId = store_code + ans;
 
                             var date = new Date(req.body.pickupDate);
                             var newDate = new Date(date.getTime() + Math.abs(date.getTimezoneOffset() * 60000));
 
+                            Timeslot.find({ 'time_Slot': req.body.timeSlot }).then((data) => {
+                                req.body.timeSlot = data[0]._id;
 
+                                serviceType.find({ 'type': req.body.servicetype }).then((servicetype) => {
+                                    req.body.servicetype = servicetype[0]._id;
+                                    req.body.franchise = franchises[0]._id;
+                                    req.body.requestId = requestId;
+                                    req.body.pickupDate = newDate;
+                                    req.body.customer = decoded._id;
+                                    // req.body.created_by = decoded._id;
+                                    // req.body.updated_by = decoded._id;
+                                    req.body.state = true;
+                                    req.body.status = true;
 
-                            serviceType.find({ 'type': req.body.serviceType }).then((serviceType) => {
+                                    // console.log('=============',req.body);
+                                    var requestOrder = new RequestOrder(req.body);
+                                    requestOrder.save().then((order) => {
+                                        var requestId = order.requestId;
 
-                                req.body.serviceType = serviceType[0]._id;
-                                req.body.franchise = franchises[0]._id;
-                                req.body.requestId = requestId;
-                                req.body.pickupDate = newDate;
-                                req.body.customer = name;
-                                req.body.created_by = decoded._id;
-                                req.body.updated_by = decoded._id;
-                                req.body.state = true;
-                                req.body.status = true;
+                                        var date = new Date(order.pickupDate);
+                                        var d = date + '';
+                                        var dateParts = d.split("GMT");
+                                        var date1 = dateParts[0].slice(0, 15);
 
-                                // console.log(req.body);
-                                var requestOrder = new RequestOrder(req.body);
-                                requestOrder.save().then((order) => {
-                                    var requestId = order.requestId;
+                                        generateMail(email,
+                                            `Dear ${name}, 
 
-                                    var date = new Date(order.pickupDate);
-                                    var d = date + '';
-                                    var dateParts = d.split("GMT");
-                                    var date1 = dateParts[0].slice(0, 15);
-
-                                    generateMail(email,
-                                        `Dear ${name}, 
-
-Your Pick up no ${requestId} with ${franchisename} is booked for ${date1} between ${order.timeSlot}.
+Your Pick up no ${requestId} with ${franchisename} is booked for ${date1} between ${data[0].time_Slot}.
                                         
 Happy Cleaning!
                                         
 Thanks,
                                         
 Team 24Klen Laundry Science`,
-                                        `Successful Request Creation ${requestId} with 24klen Laundry Science`
-                                    );
-                                    generateSms(mobile,
-                                        `Dear ${name}, Your Pick up no ${requestId} with ${franchisename} is booked for ${date1} between ${order.timeSlot}.`
-                                    )
-                                    res.status(200).json({ requestId, Success: true, Message: 'Order Placed Successfully' });
+                                            `Successful Request Creation ${requestId} with 24klen Laundry Science`
+                                        );
+                                        generateSms(mobile,
+                                            `Dear ${name}, Your Pick up no ${requestId} with ${franchisename} is booked for ${date1} between ${data[0].time_Slot}.`
+                                        )
+                                        res.status(200).json({ requestId, Success: true, Message: 'Order Placed Successfully' });
+                                    })
                                 })
                             }).catch((err) => {
                                 res.status(400).json({ err });
@@ -136,7 +136,7 @@ Team 24Klen Laundry Science`,
     .get('/requestorder/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
 
         RequestOrder.findOne({ 'requestId': req.params.id })
-            .populate('serviceType')
+            .populate('servicetype customer timeSlot')
             .then((order) => {
                 if (!order) {
                     res.status(200).json({ Success: false, Message: "Order Not Found" });
@@ -162,8 +162,8 @@ Team 24Klen Laundry Science`,
             req.body.pickupDate = newDate;
         }
 
-        serviceType.find({ 'type': req.body.serviceType }).then((serviceType) => {
-            req.body.serviceType = serviceType[0]._id;
+        serviceType.find({ 'type': req.body.servicetype }).then((servicetype) => {
+            req.body.servicetype = servicetype[0]._id;
 
             Customer.findById({ '_id': decoded._id }).then((customer) => {
                 if (req.body.locationType == "Home") {
