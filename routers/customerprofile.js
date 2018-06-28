6a1038fa1361ed78b7d2351e7c117a38738ccb15
Ashish <Ashish@espl.local>
2018-06-28 19:14:39 +0530

    change

--- routers/customerprofile.js
+++ routers/customerprofile.js
@@ -7,6 +7,7 @@ var config = require('./../config/config');
 
 var customerProfileRouter = express.Router();
 var Customer = require('./../models/customer');
+var Franchise = require('./../models/franchise');
 
 customerProfileRouter
     .get('/mprofile', passport.authenticate('jwt', { session: false }), (req, res) => {
@@ -15,11 +16,11 @@ customerProfileRouter
 
         Customer.findOne({ '_id': decoded._id }).then((user) => {
             if (!user) {
-                res.status(200).json({Success:false, Message: "You have to Logged In!!" });
+                res.status(200).json({ Success: false, Message: "You have to Logged In!!" });
             } else {
                 res.status(200).json(user);
             }
-        }).catch((err)=>{
+        }).catch((err) => {
             res.status(400).json(err);
         });
     })
@@ -27,19 +28,24 @@ customerProfileRouter
     .put('/updateprofile/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
         var id = req.params.id;
 
-        if(req.body.dob){
+        if (req.body.dob) {
             var date = new Date(req.body.dob);
             var newDate = new Date(date.getTime() + Math.abs(date.getTimezoneOffset() * 60000));
             req.body.dob = newDate;
-            }
-        Customer.findOneAndUpdate({ '_id': id }, {
-            $set:req.body
-        }, { new: true }).then((user) => {
-            if (!user) {
-                res.status(200).json({Success:false, Message: 'No User Found' });
-            }else{
-                res.status(200).json({Success: true, Message: 'Profile Updated Successfully' });
-            }
+        }
+
+        Franchise.find({ statee: true, area: { $in: [req.body.area] } }).then((franchise) => {
+            req.body.franchise = franchise[0]._id;
+
+            Customer.findOneAndUpdate({ '_id': id }, {
+                $set: req.body
+            }, { new: true }).then((user) => {
+                if (!user) {
+                    res.status(200).json({ Success: false, Message: 'No User Found' });
+                } else {
+                    res.status(200).json({ Success: true, Message: 'Profile Updated Successfully' });
+                }
+            })
         }).catch((err) => {
             res.status(400).json(err);
         })
